6b971a8cca8c7c83600c08bad7205057afdbe652
MadhushaPrasad <madushaprasad21@gmail.com>
2023-02-19 19:29:29 +0530

    feat(header):create header #2

---
+++ src/components/admin/header/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import "./header.css"
+import logo from "./../../../assets/FCSCMedia.png"
+
+function Header() {
+  return (
+    <div className="row p-0 m-0">
+      <div className="container-fluid w-100 pe-0  ps-0">
+        <header className="sticky-top d-flex flex-wrap justify-content-center py-3 pe-0">
+          <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none">
+            <div className="logoDiv d-flex justify-content-center align-items-center">
+              <div>
+                <img src={logo} alt="" width="90" />
+              </div>
+            </div>
+          </a>
+
+          <ul className="nav nav-pills">
+            <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
+            <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
+            <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
+            <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
+            <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+          </ul>
+        </header>
+      </div>
+    </div>
+  )
+}
+
+export default Header
