6119d497a21900040081aa2c410aab852a159536
Navojith <navojith22@gmail.com>
2023-03-05 11:24:41 +0530

    whoWeAreSection updated

--- src/components/client/landing/whoWeAreSection/WhoWeAreSection.jsx
+++ src/components/client/landing/whoWeAreSection/WhoWeAreSection.jsx
@@ -3,17 +3,22 @@ import './whoWeAreSection.css'
 
 const WhoWeAreSection = ({ imgSrc, description, findMoreUrl }) => {
   return (
-    <div className="container-fluid row align-items-center px-2 pt-4 pb-4 m-0 whoWeAreSection">
-      <div className="col-md-12 col-lg-7">
+    <div className="container-fluid row px-2 pt-4 pb-4 mx-auto whoWeAreSection">
+      <div className="col-md-6 col-lg-3">
         <img id="boardOfOfficialsImg" src={imgSrc} />
       </div>
-      <div className="col-md-12 col-lg-5">
-        <div className="text-section-card p-4 align-middle">
+      <div className="col-md-6 col-lg-9">
+        <div className="text-section-card p-3 p-lg-5 w-100 w-lg-50">
           <h1>Who We Are</h1>
           <p>{description}</p>
-          <a id="findMoreBtn" className="btn btn_findMore" to={findMoreUrl}>
+          <button
+            id="findMoreBtn"
+            onClick={(e) => {
+              e.preventDefault()
+              window.location.href = findMoreUrl
+            }}>
             Find More
-          </a>
+          </button>
         </div>
       </div>
     </div>
