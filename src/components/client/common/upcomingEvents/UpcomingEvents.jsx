aeaecc4d9e753e357eac9a02bc7df2de9494885a
Sanjana Dinith <sanjana.dinith.r@gmail.com>
2023-03-12 09:23:00 +0530

    Added missings (#19)

--- src/components/client/common/upcomingEvents/UpcomingEvents.jsx
+++ src/components/client/common/upcomingEvents/UpcomingEvents.jsx
@@ -1,47 +1,60 @@
 import React from 'react'
-import './upcomingEvents.css'
 // import Swiper core and required modules
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
-
 import { Swiper, SwiperSlide } from 'swiper/react'
-import heroSecImage from "../../../../assets/images/heroSecImg.svg"
+
+import state from './events.json'
 // Import Swiper styles
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
 
+import './upcomingEvents.css'
+
 function UpcomingEvents() {
+  // let data = require('./events.json')
   return (
-    <div className="container">
-      <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}>
-        <SwiperSlide>
-          <div className="b-full">
-            <div className="row">
-              <div className="col-6 ">
-                  {/* <img className="heroImgSec" src={heroSecImage} alt="heroSecImage" /> */}
-                  <div>
-                    <div class="container b-full"></div>
+    <div className="container mb-5">
+      <div className="text-center text-color pb-5">
+        <h2>
+          <b>UPCOMING EVENTS</b>
+        </h2>
+      </div>
+      <div className="shadow p-5">
+        <Swiper modules={[Navigation, Pagination]} navigation pagination={{ clickable: true }}>
+          {state.data.map((dynamicData, i) => (
+            <SwiperSlide>
+              <div className="b-full">
+                <div className="row">
+                  <div className="col-lg-5 ">
+                    <div className="image p-5 h-full w-full"></div>
+                  </div>
+                  <div className="col-lg-7 text-color align-self-center">
+                    <div className="px-0 py-5 py-lg-0 px-lg-5">
+                      <h2>{dynamicData.title}</h2>
+                      <h5 className="py-2">{dynamicData.subtitle}</h5>
+                      <h5 className="py-2">{dynamicData.date}</h5>
+                      <p className="text-dec pt-3 pt-lg-5">{dynamicData.description}</p>
+                      <h5 className="py-2">
+                        <strong>{dynamicData.hashtags}</strong>
+                      </h5>
+                      <div className="row">
+                        <div className="col-md-10">
+                          <h5 className="py-2">{dynamicData.by}</h5>
+                        </div>
+                        <div className="col">
+                          <h5 className="py-2">{dynamicData.who}</h5>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                </div>
-              <div className="col-md-6 ">
-                
               </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
-      </Swiper>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   )
 }
