a03eb50662e5e67deb9409dd29d0844a787662b6
Sahan Perera <sahanvimukthi00@gmail.com>
2023-03-20 20:43:25 +0530

    feat(header): create hamburger menu for mobile responsive header

--- src/components/client/common/header/Header.jsx
+++ src/components/client/common/header/Header.jsx
@@ -1,31 +1,63 @@
-import React from "react"
-import "./header.css"
-import logo from "../../../../assets/logo/FCSCMedia.png"
+import './header.css'
+import logo from '../../../../assets/logo/FCSCMedia.png'
+import React from 'react'
 
-function Header() {
+export default function Header() {
   return (
-    <div className="row p-0 m-0">
-      <div className="container-fluid w-100 pe-0  ps-0">
-        <header className="sticky-top d-flex flex-wrap justify-content-center py-3 pe-0">
-          <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none">
+    <header>
+      <nav class="navBarCont navbar navbar-expand-lg bg-body-tertiary">
+        <div class="navBarInnerCont container-fluid">
+          <a class="navbar-brand" href="#">
             <div className="logoDiv d-flex justify-content-center align-items-center">
               <div>
                 <img src={logo} alt="" width="90" />
               </div>
             </div>
           </a>
-
-          <ul className="nav nav-pills me-4 navUl">
-            <li className="nav-item"><a href="src/components/client/header#" className="nav-link active" aria-current="page">Home</a></li>
-            <li className="nav-item"><a href="src/components/client/header#" className="nav-link">Features</a></li>
-            <li className="nav-item"><a href="src/components/client/header#" className="nav-link">Pricing</a></li>
-            <li className="nav-item"><a href="src/components/client/header#" className="nav-link">FAQs</a></li>
-            <li className="nav-item"><a href="src/components/client/header#" className="nav-link">About</a></li>
-          </ul>
-        </header>
-      </div>
-    </div>
+          <button
+            class="navbar-light navbar-toggler m-3 dark"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="navlinkCont collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav nav-pills gap-2">
+              <li className="nav-item">
+                <a
+                  href="src/components/client/header#"
+                  className="nav-link active"
+                  aria-current="page">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="src/components/client/header#" className="nav-link">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="src/components/client/header#" className="nav-link">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="src/components/client/header#" className="nav-link">
+                  FAQs
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="src/components/client/header#" className="nav-link">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
   )
 }
-
-export default Header
