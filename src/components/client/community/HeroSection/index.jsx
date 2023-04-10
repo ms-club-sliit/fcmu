b55b6d43bc4f197f5c9a7d8d5766167748b7cf5a
MadhushaPrasad <madushaprasad21@gmail.com>
2023-04-11 00:13:04 +0530

    community page done

--- src/components/client/community/HeroSection/index.jsx
+++ src/components/client/community/HeroSection/index.jsx
@@ -3,14 +3,14 @@ import "./styles.css"
 const HeroSection = ({ description, imgAltText, imgSrc, title }) => {
   return (
     <div>
-      <div id="events-hero-section" className="container-fluid row px-2 pt-4 pb-4 m-0">
+      <div id="events-hero-section" className="container-fluid row px-2 pt-4 m-0">
         <div className="row mb-3 mt-3">
           <div className="col-lg-6 col-md-6 col-sm-12 col-12">
             <h1 id="events-hero-title">{title.titleOne}</h1> <br />
             <h1 id="events-hero-title">{title.titleTwo}</h1> <br />
             <h1 id="events-hero-title">{title.titleThree}</h1>
             <div className="col-12 col-sm-12 col-md-6 col-lg-12">
-              <p className="desc mt-5">{description}</p>
+              <p className="desc mt-5 mb-0">{description}</p>
             </div>
           </div>
           <div
