a9a31fd8d9e1fb89397c40d57fa0f65fbbb729f1
IndudiniThennakoon <thennakoonindudini@gmail.com>
2023-04-29 23:35:16 +0530

    chore(event page) : change event page content

--- src/components/client/eventsPage/HeroSection/index.jsx
+++ src/components/client/eventsPage/HeroSection/index.jsx
@@ -1,6 +1,6 @@
 import './styles.css'
 
-function HeroSection({ description, imgAltText, imgSrc, title }) {
+function HeroSection({ description,description2, imgAltText, imgSrc, title }) {
   return (
     <div>
       <div id="events-hero-section" className="container-fluid row px-2 pt-4 pb-4 m-0">
@@ -10,6 +10,7 @@ function HeroSection({ description, imgAltText, imgSrc, title }) {
         <div className="row">
           <div className="col-12 col-sm-12 col-md-6 col-lg-6">
             <p className="desc">{description}</p>
+            <p className="descrip">{description2}</p>
           </div>
           <div className="col-12 col-sm-12 col-md-6 col-lg-6 d-flex justify-content-end align-items-center mt-4 mt-lg-0 mt-md-4 mt-sm-4">
             <div id="events-hero-img-container">
--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,23 +1,24 @@
-import HeroSection from "../components/client/eventsPage/HeroSection"
-import manPlaysGuitarImg from "../assets/images/EventsPage/manPlaysGuitar.svg"
-import UpcomingEvents from "../components/client/common/upcomingEvents/UpcomingEvents"
-import LatestNotice from "../components/client/landing/latestNotice/LatestNotice"
+import HeroSection from '../components/client/eventsPage/HeroSection'
+import manPlaysGuitarImg from '../assets/images/EventsPage/manPlaysGuitar.svg'
+import UpcomingEvents from '../components/client/common/upcomingEvents/UpcomingEvents'
+import LatestNotice from '../components/client/landing/latestNotice/LatestNotice'
 
-const EventsPage = () => {
-  const lorem =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+function EventsPage() {
+    const lorem =
+        'Our media unit is a thriving community of passionate and driven individuals who are committed to creating and delivering high-quality content that inspires and informs audiences both locally and globally. We offer a range of opportunities for students to get involved in all aspects of media production, from designing graphics and websites to producing videos and podcasts.Joining our media unit is not only a chance to gain hands-on experience and build your portfolio, but also an opportunity to be part of a vibrant and dynamic community. We encourage students to share their ideas, collaborate with others, and grow their skills in a supportive and engaging environment.'
 
-  return (
-    <div>
-      <HeroSection
-        description={lorem}
-        imgAltText={"Man plays guitar image"}
-        imgSrc={manPlaysGuitarImg}
-        title={"What are events"}
-      />
+    const para = 'So what are you waiting for? If you\'re a young and talented media enthusiast who is passionate about creating and delivering engaging content, we invite you to join us at the SLIIT Faculty of Computing Media Unit. Your moment to shine is now!'
+    return (
+        <div>
+            <HeroSection
+                description={lorem}
+                description2={para}
+                imgAltText="Man plays guitar image"
+                imgSrc={manPlaysGuitarImg}
+                title="What are events"
+            />
       <LatestNotice />
       <UpcomingEvents />
-
     </div>
   )
 }
