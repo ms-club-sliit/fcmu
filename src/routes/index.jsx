251b4bc2acc88fdba53d31b1330bd8fc735c6a0e
MadhushaPrasad <madushaprasad21@gmail.com>
2023-04-29 19:54:05 +0530

    fix(footer) : fixed footer styles

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from '../components/client/common/header'
-import EventsPage from '../pages/EventsPage'
-import LandingPage from '../pages/LandingPage'
-import CommunityPage from '../pages/CommunityPage'
-import AboutPage from '../pages/AboutPage'
-import ContactUs from '../pages/ContactUs.jsx'
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Header from "../components/client/common/header"
+import EventsPage from "../pages/EventsPage"
+import LandingPage from "../pages/LandingPage"
+import CommunityPage from "../pages/CommunityPage"
+import AboutPage from "../pages/AboutPage"
+import ContactUs from "../pages/ContactUs.jsx"
+import Footer from "../components/client/common/footer/footer.jsx"
 
 function RouteComponent() {
   return (
@@ -19,6 +20,7 @@ function RouteComponent() {
         <Route path="/contactUs" element={<ContactUs />} />
         <Route path="/" element={<LandingPage />} />
       </Routes>
+      <Footer />
     </Router>
   )
 }
