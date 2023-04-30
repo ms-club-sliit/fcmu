192b63a632e33816c3c13274860ec9fb964a62b9
Navojith <navojith22@gmail.com>
2023-04-30 12:31:43 +0530

    main events updated

--- src/components/client/common/EventsComponent/mainEvents.js
+++ src/components/client/common/EventsComponent/mainEvents.js
@@ -1,25 +1,35 @@
-import img1 from '../../../../assets/images/samplePost1.webp'
+import img1 from '../../../../assets/images/EventsPage/MainEvents/MainEvent1.webp'
 import img2 from '../../../../assets/images/samplePost1.webp'
 
 export const data = [
   {
-    id: '15',
-    title: 'FRESHERS RECRUTMENT',
-    subtitle: 'Calling out to all creative Volunteers',
-    date: '03/11/2022',
+    id: '1',
+    title: 'Wiramaya - 2022',
+    subtitle: 'Organized by Faculty of Computing Student Community - Faculty of Computing - SLIIT!',
+    date: '29/10/2022',
     imgSrc: img1,
     description:
-      'MERRY CHRISTMAS 2022! , Wishing you good health, happiness, peace, hope, love, and prosperity, this holiday season and in the coming New Year. May the magic of Christmas fill your heart all year long. Design by: Gimhana Deshapriya Content by: Pabodha Oshadari',
+      'The focus on display was mixed with oriental, classical and modern performing arts with dancing, drama, and singing taking pride of place. It was a stunning showcase of what the gifted SLIIT students were capable of. Wiramaya 2022 was further enriched by a team of renowned artists such as Dhanith Sri, Dinesh Gamage, Yasas Medagedara, Susil Fernando, and Amuthu Band whose performances heightened the glamour and grandeur of the evening.',
     hashtags: [
       {
         id: '100',
         text: '#sliit',
-        link: 'https://www.google.com',
+        link: 'https://www.sliit.lk/',
       },
       {
-        id: '200',
-        text: '#sliitMedia',
-        link: '#',
+        id: '101',
+        text: '#foc',
+        link: 'https://www.sliit.lk/computing/',
+      },
+      {
+        id: '102',
+        text: '#sliitmedia',
+        link: 'https://www.facebook.com/sliit.fcmu/',
+      },
+      {
+        id: '103',
+        text: '#fcmu',
+        link: 'https://www.facebook.com/sliit.fcmu/',
       },
     ],
     by: 'BY SLIIT FCMU COMMUNITY',
